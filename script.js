3c5674cdfcea3ab5222a2ae15935d2644a550d4a
LarsMartin-Lillerovde <239625292+dalsfjord-blip@users.noreply.github.com>
2026-03-28 15:11:42 +0100

    New build for main header thru all pages with social media logos.

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 function showMenu() {
     // Finn menyen via ID-en
     var menu = document.getElementById("myMenu");
+    var social = document.getElementById("mySocialLinks");
     
-    // Legg til klassen "show"
-    menu.classList.add("show");
+   // Legg til klassen "show" på begge to
+    if (menu) {
+        menu.classList.add("show");
+    }
+    
+    if (social) {
+        social.classList.add("show");
+    }
 }
-
